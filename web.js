f2d85eb09087fefaa681cef31dffa83563c0bb88
Namrata Kannan <iamnam24@gmail.com>
2013-07-18 06:52:13 +0000

    changes to web

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs=require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+var buffer=new Buffer(20);
+buffer=fs.readFileSync('index.html');
+var string= buffer.ToString('utf8',0,buffer.length);
+
+response.send(string);
 });
 
 var port = process.env.PORT || 5000;
