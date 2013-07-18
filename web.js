ca26a56d0dbc3209ffa996fb485c1baa74a61316
Namrata Kannan <iamnam24@gmail.com>
2013-07-18 16:25:50 +0000

    more changes

--- web.js
+++ web.js
@@ -7,10 +7,11 @@ app.get('/', function(request, response) {
   
 var buffer=new Buffer(20);
 buffer=fs.readFileSync('index.html');
+var stringResponse=buffer.toString('utf8');
 
-
-response.send(buffer);
+response.send(stringResponse);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
