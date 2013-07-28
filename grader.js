62efd055d4b92dfa88e4a5d54132eed6539f9a3f
Namrata Kannan <iamnam24@gmail.com>
2013-07-28 11:39:09 +0000

    made changes to grader to take url from command line

--- grader.js
+++ grader.js
@@ -1,56 +1,145 @@
-var fs=require('fs');
-var program=require('commander');
-var cheerio=require('cheerio');
-var HTMLFILE_DEFAULT="index.html";
-var CHECKSFILE_DEFAULT="checks.json";
-var sys=require('util');
-var rest=require('restler');
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
 
-var assertFileExists=function(infile){
-var instr=infile.toString();
-if(!fs.existsSync(instr)){
-console.log("%s does not exist. Exiting.", instr);
-process.exit(1);
-}
-return instr;
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+
+var fs = require('fs');
+var program = require('commander');
+var sys = require('util'); 
+var cheerio = require('cheerio');
+var rest = require('restler');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://young-spire-2382.herokuapp.com/";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
 };
 
-var cheerioHtmlFile=function (htmlfile)
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+if(require.main == module) {
+    program
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u,--url <url_path>', 'Url to index.html', URL_DEFAULT) 
+       .parse(process.argv);
+
+
+//Defining the function to download the file from given url
+ 
+var getFileFromUrl = function(url){
+
+
+//var URL = url.toString();
+//var URL=typeof(url)
+
+//console.log(URL);
+
+var fileDownload="";
+
+rest.get(url).on('complete',function(result){
+
+if  (result instanceof Error)
 {
-return cheerio.load(fs.readFileSync(htmlfile));
-};
 
-var loadChecks= function(checksfile){
-return JSON.parse(fs.readFileSync(checksfile));
-};
-
-var checkHtmlFile=function(htmlfile, checksfile){
-$=cheerioHtmlfile(htmlfile);
-var checks=loadChecks(checksfile).sort();
-var out={};
-for(var ii in checks){
-var present=$(checks[ii].length>0;
-out[checks[ii]]=present;
-}
-return out;
-};
-
-
-var clone=function(fn){
-return fn.bind({});
-};
-
-if(require.main==module){
-
-program.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSILE_DEFAULT)
-.option('-f, --file<html_file>','Path to index.html',clone(assertFileExists), HTMLFILE_DEFAULT)
-.parse(process.argv);
-
-var checkJson=checkHtmlFile(program.file, program.checks);
-var outJson=JSON.stringify(checkJson,null,4);
-console.log(outJson);
+  sys.put('Error:' + result.message);
+  this.retry(500);   
 }
 else{
-exports.checkHtmlFile=checkHtmlFile;
+
+fs.writeFile('newfile.html',result,function(err) {
+
+if(err) throw err;
+//console.log('Saved!');
+
+fileDownload="newfile.html";
+
+console.log(fileDownload);
+//return fileDownload;
+
+var checkJson = checkHtmlFile(fileDownload, program.checks);
+var outJson = JSON.stringify(checkJson, null, 4);
+console.log(outJson);
+fs.writeFile('JSON.txt',outJson);
+
+});
+
 }
- 
+
+}); 
+
+};
+     
+
+
+if (program.url != null)
+{
+getFileFromUrl(program.url);
+console.log(program.url);
+}
+
+//console.log(file);
+
+else
+{
+ var checkJson = checkHtmlFile(program.file, program.checks);
+ var outJson = JSON.stringify(checkJson, null, 4);
+console.log(outJson);
+
+}
+
+
+
+
+    
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
